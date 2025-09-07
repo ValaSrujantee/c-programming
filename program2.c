a1352b2d40c60ffdfc8fea30389e04b77e9229d4
Srujantee vala <sruvala333@gmail.com>
2025-09-07 11:27:30 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int l , b , a , p ;
+    l = 70 ;
+    b = 90 ;
+    a = l * b ;
+    p = 2 * ( l + b );
+    printf ("area = %d \n",a);
+    printf ("perameter = %d \n",p);
+
+}
