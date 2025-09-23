d616c6898735c703555cbd2bbd3902c87bc1ac62
Srujantee vala <sruvala333@gmail.com>
2025-09-23 10:03:45 +0530

    Add files via upload

---
+++ program19(a).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    for (int i=0 ; i<5 ; i++)
+    {
+        for (int j=0 ; j<=i ; j++)
+        {
+            if (j%2==0)
+            {
+                  printf(" 1");
+            }
+            else
+            {
+                printf(" 0");
+            }
+        }
+         printf("\n");
+    }
+}
